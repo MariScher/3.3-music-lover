87724a7cdbfbd50b3189c340ee65fb7f5dcf7024
MariScher <marina_vetr@mail.ru>
2022-03-02 21:07:16 +0300

    Change II

--- src/main/kotlin/3.3-music-lover.kt
+++ src/main/kotlin/3.3-music-lover.kt
@@ -1,19 +1,19 @@
 fun main() {
-    val totalPrice = 15000.00
+    val totalPrice = 15000
     val monthlyBuyer = true
 
     val standardDiscount = 100
     val increasedDiscount = 0.95
     val monthlyDiscount = 0.99
-    val discountStart = 1001
-    val discountRise = 10_001
+    val discountStart = 1000
+    val discountRise = 10_000
 
 
-    val result = if (totalPrice >= discountRise) totalPrice * increasedDiscount
-    else if (totalPrice >= discountStart) totalPrice - standardDiscount
+    val result = if (totalPrice > discountRise) (totalPrice * increasedDiscount).toInt()
+    else if (totalPrice > discountStart) totalPrice - standardDiscount
     else totalPrice
 
-    val totalResult = if (monthlyBuyer) result * monthlyDiscount else result
+    val totalResult = if (monthlyBuyer) (result * monthlyDiscount).toInt() else result
 
     println("Итого: $totalResult")
 }
